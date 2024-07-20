0f5e8796bd692e8b2d55a10eee2638a0e1e82dd8
Miranda Henning <35973769+mhenning3@users.noreply.github.com>
2024-07-19 21:18:03 -0400

    added fire sequence
    will fix bullets

--- Assets/Scripts/towerBehavior.cs
+++ Assets/Scripts/towerBehavior.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class towerBehavior : MonoBehaviour
 {
-    public float AttackRadius;
+
     SphereCollider myCollider;
+    public float AttackRadius;
+    //per second
+    public float fireRate;
     // Start is called before the first frame update
+    List<int> list = new List<int>();
     void Start()
     {
         myCollider=GetComponent<SphereCollider>();
         myCollider.radius=AttackRadius;
         Debug.Log("hello");
+
+
     }
-    
-    List<int> list = new List<int>();
+
+
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log(other+" "+other.GetInstanceID()+" entered");
         list.Add(other.GetInstanceID());
         //printList(list);
+        StartCoroutine(startFire(other.gameObject));
 
     }
     private void OnTriggerExit(Collider other)
@@ -27,7 +35,7 @@ public class towerBehavior : MonoBehaviour
         //Debug.Log(other + " " + other.GetInstanceID()+ " exited");
         list.Remove(other.GetInstanceID());
         //printList(list);
-        
+        StopCoroutine(startFire(other.gameObject));
 
     }
     //for testing
@@ -41,6 +49,20 @@ public class towerBehavior : MonoBehaviour
         numbs += " }";
         Debug.Log(numbs);
 
+    }
+    //pick a random enemy to attack
+    int enemyID;
+
+    private IEnumerator startFire(GameObject unit)
+    {
+        enemyID = Random.Range(0, list.Count-1);
+        while (true)
+        {
+            Debug.Log("PEW! hit enemy "+ enemyID+ " with id "+ unit.GetComponent<Collider>().GetInstanceID()+ " at location "+ unit.transform.position);
+            yield return new WaitForSeconds(fireRate);
+        }
+
+        
     }
     // Update is called once per frame
     void Update()
