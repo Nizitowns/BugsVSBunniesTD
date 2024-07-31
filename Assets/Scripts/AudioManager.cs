bbf9c42f379d728e481e5997c66307189a9cd835
Eric Patrick <ericmpatrick777@gmail.com>
2024-07-31 13:08:15 -0700

    Persistent Audio Volumes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,13 +6,25 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     public static float SFXVolume = 0.5f;
+    public static float MusicVolume = 0.5f;
 
+
+    private void Start()
+    {
+        SFXVolume = PlayerPrefs.GetFloat("SFX_Vol", 0.5f);
+        MusicVolume = PlayerPrefs.GetFloat("Music_Vol", 0.5f);
+    }
     public void SetSFXVolume(Slider slider)
     {
         SFXVolume = slider.value/slider.maxValue;
+
+        PlayerPrefs.SetFloat("SFX_Vol", SFXVolume);
     }
     public void SetMusicVolume(Slider slider)
     {
-        SFXVolume = slider.value / slider.maxValue;
+        MusicVolume = slider.value / slider.maxValue;
+
+
+        PlayerPrefs.SetFloat("Music_Vol", MusicVolume);
     }
 }
