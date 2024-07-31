68871589db38fcf641d1401772ebd8256df56e6d
Eric Patrick <ericmpatrick777@gmail.com>
2024-07-31 13:07:11 -0700

    AudioManagerHookup

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,8 +7,12 @@ public class AudioManager : MonoBehaviour
 {
     public static float SFXVolume = 0.5f;
 
-    public void SetVolume(Slider slider)
+    public void SetSFXVolume(Slider slider)
     {
         SFXVolume = slider.value/slider.maxValue;
+    }
+    public void SetMusicVolume(Slider slider)
+    {
+        SFXVolume = slider.value / slider.maxValue;
     }
 }
