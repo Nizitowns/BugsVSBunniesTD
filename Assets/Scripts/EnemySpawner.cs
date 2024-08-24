d9f1a7b246246cdbdc017c2e6b08f5b57d586f2e
Eric Patrick <ericmpatrick777@gmail.com>
2024-08-24 11:56:30 -0700

    Enemy Spawner Percentages Implemented

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -16,6 +16,29 @@ public class EnemySpawner : MonoBehaviour
     [Tooltip("The delay before declaring all waves completed."),Min(0)]
     public float PostWaveDelay;
 
+    [Tooltip("On a scale from 0-1 returns how close the current wave is to completion.")]
+    public float WavePercentage()
+    {
+        if(spawnRequests.Count <= 0)
+        {//If there are no spawn requests we assume 100% completion of  this wave. (Last wave ends and we have no requests left)
+            return 1;
+        }
+        SpawnRequest current = spawnRequests[0];
+        if (current.WaitUntilCompletion)
+        {
+            return (enemiesSpawnedThisWave / current.SpawnAmount)/2.0f +  (1-Mathf.Min(Mathf.Max(0,((float)transform.childCount/(float)current.SpawnAmount)),1))/2.0f;
+        }
+        else
+        {//The wave ends when all enemies spawn
+            return enemiesSpawnedThisWave / current.SpawnAmount;
+        }
+    }
+    [Tooltip("On a scale from 0-1 returns how close this entire enemy spawner is to 100% completion.")]
+    public float TotalPercentage()
+    {
+        return totalWavesCompleted/ totalWavesToSpawn;
+    }
+
     [System.Serializable]
     public class SpawnRequest
     {
@@ -48,16 +71,23 @@ public class EnemySpawner : MonoBehaviour
     {
         return transform.childCount <= 0;
     }
+    float enemiesSpawnedThisWave;
+    float totalWavesCompleted;
+    float totalWavesToSpawn;
     public IEnumerator EnemySpawnerLoop()
     {
         while (spawnRequests.Count > 0)
         {
-
+            if(spawnRequests.Count> totalWavesToSpawn)
+            {
+                totalWavesToSpawn = spawnRequests.Count;
+            }
             WaveStarted?.Invoke();
             SpawnRequest current = spawnRequests[0];
+            enemiesSpawnedThisWave = 0;
             for (int i = 0; i < current.SpawnAmount; i++)
             {
-
+                enemiesSpawnedThisWave++;
                 Vector3 spawnPos = transform.position;
                 PathNode initTarget = null;
 
@@ -90,6 +120,7 @@ public class EnemySpawner : MonoBehaviour
             {
                 yield return new WaitUntil(hasNoChildren);
             }
+            totalWavesCompleted++;
             WaveEnded?.Invoke();
 
             yield return new WaitForSeconds(current.PostWaveDelay);
