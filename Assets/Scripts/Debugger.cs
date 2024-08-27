ba498ddc6a1a616565fdaceccf40ae71185eaeda
Kadir Kul <56093519+kkul40@users.noreply.github.com>
2024-08-27 16:13:48 -0400

    Prefabs

---
+++ Assets/Scripts/Debugger.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace DefaultNamespace
+{
+    public class Debugger
+    {
+        public static bool ShowPaths = false;
+        public static bool ShowObstacleFacingDirection = false;
+       
+    }
+}
---
+++ Assets/Scripts/PathTileObject.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace DefaultNamespace
+{
+    [SelectionBase]
+    public class PathTileObject : MonoBehaviour
+    {
+        public bool HasObstacle = false;
+        
+        public DirectionData directionData = new DirectionData();
+        
+        
+        private void OnDrawGizmos()
+        {
+            if (!Debugger.ShowObstacleFacingDirection) return;
+            
+            Gizmos.color = Color.green;
+            Vector3 startPos = transform.position + (transform.up * 3);
+            switch (directionData.eDirection)
+            {
+                case eDirection.Forward:
+                    Gizmos.DrawRay(startPos, Vector3.forward * 5);
+                    break;
+                case eDirection.Back:
+                    Gizmos.DrawRay(startPos, Vector3.back * 5);
+                    break;
+                case eDirection.Right:
+                    Gizmos.DrawRay(startPos, Vector3.right * 5);
+                    break;
+                case eDirection.Left:
+                    Gizmos.DrawRay(startPos, Vector3.left * 5);
+                    break;
+            }
+        }
+    }
+}
