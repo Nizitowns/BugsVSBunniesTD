df21432583d913bc5dbe666a9e709b45063cbcd6
Eric Patrick <ericmpatrick777@gmail.com>
2024-08-29 11:33:57 -0700

    Copy Component Fixed

--- Assets/Scripts/Helper/ComponentCopier.cs
+++ Assets/Scripts/Helper/ComponentCopier.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -7,11 +10,45 @@ namespace Helper
     {
         public static T CopyComponent<T>(T original, GameObject destination) where T : Component
         {
-            var type = original.GetType();
-            var copy = destination.AddComponent(type);
-            var fields = type.GetFields();
-            foreach (var field in fields) field.SetValue(copy, field.GetValue(original));
-            return copy as T;
+            System.Type type = original.GetType();
+
+            var dst = destination.GetComponent(type) as T;
+            if (!dst) dst = destination.AddComponent(type) as T;
+
+            var fields = GetAllFields(type);
+            foreach (var field in fields)
+            {
+                if (field.IsStatic || IsObsolete(field)) continue;
+                field.SetValue(dst, field.GetValue(original));
+            }
+
+            var props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                if (!prop.CanWrite || !prop.CanWrite || prop.Name == "name" || IsObsolete(prop)) continue;
+                prop.SetValue(dst, prop.GetValue(original, null), null);
+            }
+
+            return dst as T;
+        }
+
+        public static IEnumerable<FieldInfo> GetAllFields(System.Type t)
+        {
+            if (t == null)
+            {
+                return Enumerable.Empty<FieldInfo>();
+            }
+
+            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+                                 BindingFlags.Static | BindingFlags.Instance |
+                                 BindingFlags.DeclaredOnly;
+            return t.GetFields(flags).Concat(GetAllFields(t.BaseType));
+        }
+
+        private static bool IsObsolete(MemberInfo member)
+        {
+            var obsoleteAttr = member.GetCustomAttribute<ObsoleteAttribute>();
+            return obsoleteAttr != null;
         }
     }
 }
