fc8b1adf6059cf864ddefda755ba4629ef067098
Kadir Kul <56093519+kkul40@users.noreply.github.com>
2024-08-30 15:43:27 -0400

    Fixed Tween Leak

--- Assets/Scripts/Helper/Vector3Extension.cs
+++ Assets/Scripts/Helper/Vector3Extension.cs
@@ -5,17 +5,18 @@ namespace DefaultNamespace
 {
     public static class AnimationExtension
     {
+        public static Tween routine;
         public static void AnimatedPlacement(this GameObject gameObject)
         {
             Vector3 placedPosition = gameObject.transform.position;
             gameObject.transform.position -= Vector3.down * 2;
-            gameObject.transform.DOMove(placedPosition, 1f).SetEase(Ease.OutExpo);
+            routine = gameObject.transform.DOMove(placedPosition, 1f).SetEase(Ease.OutExpo).OnComplete(() => routine?.Kill());;
         }
 
         public static void AnimatedRemove(this GameObject gameObject)
         {
             Vector3 endPos = gameObject.transform.position + Vector3.up * 1f;
-            gameObject.transform.DOMove(endPos, 0.09f).SetEase(Ease.Linear);
+            routine = gameObject.transform.DOMove(endPos, 0.09f).SetEase(Ease.Linear).OnComplete(() => routine?.Kill());
         }
     }
 }
