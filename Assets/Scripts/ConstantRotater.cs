c26b989f8f100c6a030f3db92accbb79ecd903fa
Eric Patrick <ericmpatrick777@gmail.com>
2024-09-02 10:27:10 -0700

    Level2 Shrimp Rocks

--- Assets/Scripts/ConstantRotater.cs
+++ Assets/Scripts/ConstantRotater.cs
@@ -8,12 +8,22 @@ public class ConstantRotater : MonoBehaviour
     float dy;
 
     public ParticleSystem OnlyRotateIfPlaying;
+    public enum RotateType {DYOnly,RandomizeTotal };
+    public RotateType myType=RotateType.DYOnly;
     void Update()
     {
-        if (OnlyRotateIfPlaying == null || OnlyRotateIfPlaying.isPlaying)
+        if (myType == RotateType.DYOnly)
         {
-            dy += Speed * Time.fixedDeltaTime * Time.timeScale;
-            transform.localEulerAngles = new Vector3(dy, 0, 0);
+            if (OnlyRotateIfPlaying == null || OnlyRotateIfPlaying.isPlaying)
+            {
+                dy += Speed * Time.fixedDeltaTime * Time.timeScale;
+                transform.localEulerAngles = new Vector3(dy, 0, 0);
+            }
+        }
+        else if(myType== RotateType.RandomizeTotal) {
+            GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1, 1.0f)*Speed, Random.Range(-1, 1.0f) * Speed, Random.Range(-1, 1.0f) * Speed));
+
+            transform.localEulerAngles = new Vector3(Mathf.Clamp(transform.localEulerAngles.x,40,120), transform.localEulerAngles.y, transform.localEulerAngles.z);
         }
     }
 }
